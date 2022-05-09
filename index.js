557d8517b263179df208a54b496a7ea296d0c806
JubiLeed <102115118+JubiLeed@users.noreply.github.com>
2022-05-09 13:40:21 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const name = document.querySelector("#child")
+const greetChild = document.querySelector(".theChild")
+const greetOutput = document.querySelector(".greeting")
+
+greetChild.addEventListener('click', (event) => {
+    alert("Suck it");
+})
+
